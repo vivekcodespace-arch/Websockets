e0755f42b9ac81647eb734d5fbd2d1bc98f6589b
Vivek Sharma <vivek.codespace@gmail.com>
2026-02-15 19:25:13 +0530

    created a websocket server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import { WebSocketServer } from "ws";
- 
-const wss = new WebSocketServer({port : 8080});
 
-//event handler
-wss.on("connection",function(socket){
-  console.log("hello word")
-  socket.send(`The Current price of solana is ${Math.random()}`)
-  // setInterval(() => {
-  //   socket.send("There is a crime in the world..")
-  // }, 500);
 
+
+const wss = new WebSocketServer({port:8080})
+
+//This acts as a server 
+
+wss.on("connection", function(socket){
+  console.log("User connected.")
+
+  
   socket.on("message",(e)=>{
-    if(e.toString() === "ping"){
+    console.log(`${e.toString()} message was received from the client.`)
+
+    if(e.toString() === 'ping'){
       socket.send("pong")
     }
   })
-
 })
